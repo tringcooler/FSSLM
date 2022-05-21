d6283d1f2a17c7c2b0571a7cf4cae0a9879ab7b7
tringcooler <tringcooler@users.noreply.github.com>
2022-05-21 20:33:54 +0800

    Update test_case.js

--- test_case.js
+++ test_case.js
@@ -39,12 +39,23 @@ const TEST_CASE = ((...c_sslms) => {
         }
     }
     
+    function *randpick(src) {
+        let seq = src.slice();
+        let slen;
+        while((slen = seq.length) > 0) {
+            let r = Math.floor(Math.random() * slen);
+            yield seq[r];
+            seq.splice(r, 1);
+        }
+    }
+    
     class c_test_route {
         
         constructor(tseq, qseq) {
             this.sslms = c_sslms.map(c => new c(true, true));
             this.tseq = tseq;
             this.qseq = qseq;
+            this.stat = {};
             this.nid = 0;
         }
         
@@ -54,6 +65,11 @@ const TEST_CASE = ((...c_sslms) => {
                 let nid = ++this.nid;
                 sslm.set(vset, nid);
             }
+        }
+        
+        raise_conflict() {
+            window.conflict_route = this;
+            throw Error('conflict: detail in conflict_route');
         }
         
         minfo_cmp(m1, m2) {
@@ -69,44 +85,94 @@ const TEST_CASE = ((...c_sslms) => {
                     }
                 }
             }
-            if(!eq) {
-                throw Error('conflict');
-            }
+            return eq;
         }
         
         match() {
             let qseq = this.qseq;
             let sslms = this.sslms;
             for(let tset of qseq) {
+                this.stat.query_set = tset;
                 let lst_minfo = null;
                 let lst_r_minfo = null;
                 for(let sslm of sslms) {
                     let minfo = sslm.match(tset, false);
-                    this.minfo_cmp(minfo, lst_minfo);
+                    if(!this.minfo_cmp(minfo, lst_minfo)) {
+                        this.raise_conflict();
+                    }
                     lst_minfo = minfo;
                     minfo = sslm.match(tset, true);
-                    this.minfo_cmp(minfo, lst_r_minfo);
+                    if(!this.minfo_cmp(minfo, lst_r_minfo)) {
+                        this.raise_conflict();
+                    }
                     lst_r_minfo = minfo;
                 }
             }
         }
         
-        step(tset) {
-            this.set(tset);
-            this.match();
-        }
-        
-        run() {
+        *step() {
             let tseq = this.tseq;
             for(let tset of tseq) {
-                step(tset);
+                this.stat.test_set = tset;
+                this.set(tset);
+                this.match();
+                yield tset;
             }
         }
         
     }
     
-    return n => {
+    const asleep = ms => new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+    
+    class c_breakable {
         
+        constructor() {
+            this.to_break = false;
+            this.hook();
+        }
+        
+        hook() {
+            window.addEventListener('keydown', event => {
+                if (event.ctrlKey && event.code === 'KeyC') {
+                    this.to_break = true
+                }
+            });
+        }
+        
+        async check(t = 0) {
+            await asleep(t);
+            if(this.to_break) {
+                this.to_break = false;
+                return true;
+            }
+            return false;
+        }
+        
+    }
+    const G_BREAK = new c_breakable();
+    
+    return async n => {
+        let all_set = [...Array(n)].map((v, i) => i + 1);
+        let comb_seq = [];
+        for(let s of combine(all_set)) {
+            if(s.length === 0) continue;
+            comb_seq.push(s);
+        }
+        console.log(`test start for ${n} elments. ctrl + c to break.`);
+        let idx = 0;
+        while(true) {
+            console.log(`route ${++idx}`);
+            let route = new c_test_route(randpick(comb_seq), comb_seq);
+            for(let _ of route.step()) {
+                //console.log('.');
+            }
+            if(await G_BREAK.check()) {
+                break;
+            }
+        }
+        console.log('break');
     };
     
 })();
