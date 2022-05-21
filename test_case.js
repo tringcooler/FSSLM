606921e726147424635c238959e38ef7b119d7b6
tringcooler <tringcooler@users.noreply.github.com>
2022-05-21 08:00:57 +0800

    Update test_case.js
    用上之前的masked arr后，内存使用情况有所好转。但是排列组合后数量还是太过庞大，仅仅4个元素排列组合都无法运算完成。
    所以准备放弃在测视例中排列组合的做法，转为使用随机样例的做法。
    因此这次提交不予使用，仅仅作为记录保存。

--- test_case.js
+++ test_case.js
@@ -1,15 +1,104 @@
 const TEST_CASE = ((...c_sslms) => {
+    
+    const [
+        PL_MA_SRC, PR_MA_MSK, PR_MA_LEN,
+        MTD_MA_INT,
+    ] = (function*() {
+        while(true) {
+            yield Symbol();
+        }
+    })();
+    
+    class c_masked_arr {
+            
+        constructor(src) {
+            this[PL_MA_SRC] = src;
+            this[PR_MA_MSK] = this[MTD_MA_INT](0);
+            this[PR_MA_LEN] = src.length;
+        }
+        
+        clone() {
+            let r = new c_masked_arr(this[PL_MA_SRC]);
+            r[PR_MA_MSK] = this[PR_MA_MSK];
+            r[PR_MA_LEN] = this[PR_MA_LEN];
+            return r;
+        }
+        
+        get length() {
+            return this[PR_MA_LEN];
+        }
+        
+        get src() {
+            return this[PL_MA_SRC];
+        }
+        
+        [MTD_MA_INT](v) {
+            if(this[PL_MA_SRC].length > 52) {
+                return BigInt(v);
+            } else {
+                return v;
+            }
+        }
+        
+        *coiter() {
+            let src = this[PL_MA_SRC];
+            let msk = this[PR_MA_MSK];
+            let v0 = this[MTD_MA_INT](0);
+            let v1 = this[MTD_MA_INT](1);
+            for(let i = v0; i < src.length; i++) {
+                let m = ((msk >> i) & v1);
+                if(m) continue;
+                let v = src[i];
+                let comsk = (msk | (v1 << i));
+                let coarr = new c_masked_arr(src);
+                coarr[PR_MA_MSK] = comsk;
+                coarr[PR_MA_LEN] = this[PR_MA_LEN] - 1;
+                yield [v, coarr];
+            }
+        }
+        
+        *[Symbol.iterator]() {
+            for(let [v, _] of this.coiter()) {
+                yield v;
+            }
+        }
+        
+        merge(dst) {
+            let src = this[PL_MA_SRC];
+            if(src !== dst[PL_MA_SRC]) {
+                throw Error('merge with different sources');
+            }
+            let smsk = this[PR_MA_MSK];
+            let dmsk = dst[PR_MA_MSK];
+            this[PR_MA_MSK] = (smsk | dmsk);
+            this[PR_MA_LEN] -= sparse_popcnt(~smsk & dmsk, this[MTD_MA_INT](1));
+            return this;
+        }
+        
+        inverse() {
+            let v1 = this[MTD_MA_INT](1);
+            let slen = this[PL_MA_SRC].length;
+            this[PR_MA_MSK] = ~this[PR_MA_MSK] & ((v1 << this[MTD_MA_INT](slen)) - v1);
+            this[PR_MA_LEN] = slen - this[PR_MA_LEN];
+            return this;
+        }
+        
+        empty() {
+            let v1 = this[MTD_MA_INT](1);
+            let slen = this[PL_MA_SRC].length;
+            this[PR_MA_MSK] = (v1 << this[MTD_MA_INT](slen)) - v1;
+            this[PR_MA_LEN] = 0;
+            return this;
+        }
+        
+    }
 
     function *shuffle(src) {
-        let slen = src.length;
-        if(slen <= 1) {
-            yield src;
+        if(src.length < 1) {
+            yield [];
             return;
         }
-        for(let i = 0; i < slen; i++) {
-            let hd = src[i];
-            let tl = src.slice();
-            tl.splice(i, 1);
+        for(let [hd, tl] of src.coiter()) {
             for(let stl of shuffle(tl)) {
                 // faster than [hd].concat(stl)
                 yield stl.concat([hd]);
@@ -21,7 +110,7 @@ const TEST_CASE = ((...c_sslms) => {
     function *combine(src) {
         let slen = src.length;
         if(slen < 1) {
-            yield src;
+            yield [];
             return;
         }
         let [hd, ...tl] = src;
@@ -31,6 +120,15 @@ const TEST_CASE = ((...c_sslms) => {
         }
     };
     window.combine = combine;
+    
+    window.test1 = (arr) => {
+        let c = new c_masked_arr([...combine(arr)]);
+        let cnt = 0;
+        for(let itm of shuffle(c)) {
+            cnt++;
+        }
+        return cnt;
+    }
     
     class c_test_route {
         
